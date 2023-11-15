3b8c98523acd742c4cd7554171ded2532f361dc1
MegalodonGuy <megalodon.boy.42@gmail.com>
2023-11-14 23:56:01 -0500

    Imported Asset and included Stand In player and movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public int jumpPower;
+
+    public float riseDecceleration= 0.5f;
+    private float yMovement=0; 
+    private Rigidbody2D rb; 
+    private Vector2 movementDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if(yMovement>0){
+            yMovement-=riseDecceleration;
+        }
+        movementDirection= new Vector2 (Input.GetAxis("Horizontal"),0);
+        if(Input.GetButtonDown("Jump")&&rb.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            yMovement=jumpPower;
+        }
+    }
+
+    void FixedUpdate(){
+        rb.velocity=new Vector2((movementDirection*speed).x,yMovement);
+    }
+}
